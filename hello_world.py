fd44531eca5f75b4397e8ba14ac410c63d71662d
douglholmes <78378906+douglholmes@users.noreply.github.com>
2021-05-16 18:49:59 -0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+# 1
+print("Hello World!")
+
+#2
+name = "Doug"
+print("Hello", name, "!")
+print("Hello " + name + "!")
+
+#3
+name = 42
+print("Hello", name, "!")
+print("Hello " + str(name)+ "!")
+
+#4
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}.".format(fave_food1, fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}.")
