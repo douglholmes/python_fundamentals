bc4558d10d0aa8066c29ed4f4c8a3a60b1c272de
Doug Holmes <78378906+douglholmes@users.noreply.github.com>
2021-06-06 12:15:33 -0500

    Add files via upload

---
+++ user.py
@@ -0,0 +1,42 @@
+class User:
+    def __init__(self, username, email_address):
+        self.name = username
+        self.email = email_address
+        self.account_balance = 0
+    def make_deposit(self, amount):
+        self.account_balance += amount
+    def make_withdrawal(self, amount):
+        self.account_balance -= amount
+    def diplay_user_balance(self):
+        print(f"User: {self.name}, Balance: ${self.account_balance}")
+    
+    #bonus assignment that I was working to figure out
+    # def transfer_money(self, other_user , amount):
+    #     self.account_balance -= amount
+    #     # other_user = self.make_deposit(self.account_balance + amount)
+    #     print(f"{self.account_balance})
+
+
+guido = User("Guido van Rossum", "guido@python.com")
+monty = User("Monty Python", "monty@python.com")
+john = User("John Smith", "johnsmith@aol.com")
+#first user
+guido.make_deposit(100)
+guido.make_deposit(100)
+guido.make_deposit(100)
+guido.make_withdrawal(25)
+guido.diplay_user_balance()
+guido.transfer_money(john,100)
+#second user
+monty.make_deposit(500)
+monty.make_deposit(100)
+monty.make_withdrawal(50)
+monty.make_withdrawal(100)
+monty.diplay_user_balance()
+
+#third user
+john.make_deposit(900)
+john.make_withdrawal(100)
+john.make_withdrawal(100)
+john.make_withdrawal(100)
+john.diplay_user_balance()
